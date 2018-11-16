6ece4d4850368b52a782d7b6a92cdfcb3650426c
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-11-16 23:04:24 +0900

    initial commit

---
+++ MyPhoton/Assets/Scripts/InputController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InputController : MonoBehaviour {
+
+    public InputField inputField;
+
+    // 入力が完了したら呼ばれる(Enterや入力領域以外をクリック)
+    public void OnEndEdit () {
+        Debug.Log("入力完了");
+    }
+}
---
+++ MyPhoton/Assets/Scripts/Network.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+
+public class Network : MonoBehaviour {
+ 
+    void Start() {
+        // Photonサーバーに接続する(引数でゲームのバージョンを指定できる)
+        // 前準備で設定ファイルのAuto-Join Lobbyをtrueにしていると、ここでそのままロビーに接続
+        PhotonNetwork.ConnectUsingSettings(null);
+    }
+ 
+    // ロビーに接続すると呼ばれる
+    void OnJoinedLobby() {
+        Debug.Log("ロビーに入りました。");
+ 
+        // ルームに入室する
+        PhotonNetwork.JoinRandomRoom();
+    }
+ 
+    // ルームに入室すると呼ばれる
+    void OnJoinedRoom() {
+        Debug.Log("ルームへ入室しました。");
+    }
+ 
+    // ルームの入室に失敗すると呼ばれる
+    void OnPhotonRandomJoinFailed() {
+        Debug.Log("ルームの入室に失敗しました。");
+ 
+        // ルームがないと入室に失敗するため、ルームがない場合は自分で作る
+        PhotonNetwork.CreateRoom("myRoomName");
+    }
+}
